26bd5c99f30de77201f8cd1949a35db8ed4e10f8
Julian Häußler <70056318+JulianHaeussler@users.noreply.github.com>
2023-07-10 20:35:27 +0200

    Add files via upload

---
+++ Code/00_extract_verb_phrase_offsets/preprocessing_Naturalismus.py
@@ -0,0 +1,20 @@
+import glob
+import os
+import re
+
+# get paths list and novel titles
+
+path_data = r"C:\Users\Julian\HESSENBOX-DA\Projekte\Konflikte_Preprocessing\Korpora\Naturalismus"
+
+paths_list = glob.glob(os.path.join(os.getcwd(), path_data, "*.txt"))
+
+novels_titles = []
+
+for entry in paths_list:
+    novels_titles.append(re.search(r"(?<=Naturalismus\\)(.*)(?=.txt)",entry).group(1))
+
+# call preprocessing script for each novel in corpus
+
+for i in range(0,len(paths_list)):
+    title = novels_titles[i]
+    os.system("preprocess.py preprocess --parser=spacy " + title + ".txt " + title + ".json")
